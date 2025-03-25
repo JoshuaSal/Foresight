2ecee8a532f562dfc7dd9756deb3711be995d10c
JoshuaSal <joshuasalumbides21@gmail.com>
2025-03-25 02:15:12 -0400

    improved css, made default input for score and total 0 and 1, also debugged over submiting/overcalculating error

--- Foresight.js
+++ Foresight.js
@@ -1,10 +1,15 @@
-
+var submit = 0;
 function Assessment_declare() {
-    let Assessment_no = document.getElementById("Assessment_no").value;
-    Assessment_set(Assessment_no)
+    if (submit == 0) {
+        let Assessment_no = document.getElementById("Assessment_no").value;
+        submit++;
+        Assessment_set(Assessment_no)
+    } else {
+        document.getElementById("Assessment_no").disabled = true;
+    }
 }
 
-
+var collect = 0;
 function Assessment_set(x) {
     const assessmentInputsDiv = document.getElementById("Assessment_input");
 
@@ -30,6 +35,7 @@ function Assessment_set(x) {
         const scoreCell = document.createElement("td");
         const inputScore = document.createElement("input");
         inputScore.type = "number";
+        inputScore.value = "0";
         inputScore.id = `assessmentScore_${i}`;
         inputScore.name = `assessmentScore_${i}`;
         scoreCell.appendChild(inputScore);
@@ -40,6 +46,7 @@ function Assessment_set(x) {
         const totalCell = document.createElement("td");
         const inputTotal = document.createElement("input");
         inputTotal.type = "number";
+        inputTotal.value = "1";
         inputTotal.id = `assessmentTotal_${i}`;
         inputTotal.name = `assessmentTotal_${i}`;
         totalCell.appendChild(inputTotal);
@@ -67,32 +74,49 @@ function Assessment_set(x) {
 
     const submitbutton = document.createElement("BUTTON");
     submitbutton.onclick = function () {
-        collect_Assessment(x);
+        if (collect == 0) {
+            collect_Assessment(x);
+            collect++;
+        }
     };
+
     submitbutton.id = "collectbutton";
     submitbutton.appendChild(calculateimg);
     const parentElement = document.getElementById("collectbutton");
     parentElement.appendChild(submitbutton);
- 
+
 }
 
 function collect_Assessment(x) {
+
+    let totalweight = 0;
+    for (let i = 1; i <= x; i++) {
+        let countweight = document.getElementById(`assessmentWeight_${i}`).value;
+        totalweight += parseInt(countweight);
+    }
+    console.log(totalweight);
+    if (totalweight != 100) {
+        window.confirm("Warning! Weight does not equate to 100%, calculation may be off. Proceed?");
+    }
 
     let Finaltotal = 0;
     for (let i = 1; i <= x; i++) {
         let score = document.getElementById(`assessmentScore_${i}`).value;
         let total = document.getElementById(`assessmentTotal_${i}`).value;
         let weight = document.getElementById(`assessmentWeight_${i}`).value;
-
         Finaltotal += (score / total) * weight;
     }
     const FinalGrade = document.createElement("h2");
     FinalGrade.innerText = "Final grade: " + (Math.round(Finaltotal * 100) / 100).toFixed(2) + "%";
     const final = document.getElementById("finalgrade");
+    FinalGrade.id = "resultgrade";
     final.appendChild(FinalGrade);
 
     const Gradelost = document.createElement("h3")
     Gradelost.innerText = "Lost grade: " + ((Math.round((100 - Finaltotal) * 100) / 100).toFixed(2)) + "%";
     const lost = document.getElementById("lostgrade");
     lost.appendChild(Gradelost);
+    Gradelost.id = "gradelost"
+
+
 }
